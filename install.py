dfbc8c6355e6beb95a3805e5e72e90fa9b90c6f3
D.ethereal <dethereal@protonmail.com>
2021-07-14 19:14:56 +0430

    Added awesome config

--- install.py
+++ install.py
@@ -1,2 +1,6 @@
 import os
-confighome = "~/.config" # if yours is differenet, change this variable.
+confighome = "~/.config"# if yours is differenet, change this variable.
+print("Backing up .config/awesome")
+os.system("mv "+confighome+"/awesome "+confighome+"/awesome-backedup")
+os.system("cp -r awesome "+confighome+"/")
+
