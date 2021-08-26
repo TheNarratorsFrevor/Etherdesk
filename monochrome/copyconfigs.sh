5e423938b119fba23d64a91f4c936621b335e40e
DisheartenedEthereal <dethereal@protonmail.com>
2021-08-26 17:21:52 +0430

    Change to files structre

---
+++ monochrome/copyconfigs.sh
@@ -0,0 +1,8 @@
+rm -rf bspwm sxhkd polybar tmux alacritty
+
+cp -r /home/dethereal/.config/bspwm .
+cp -r /home/dethereal/.config/sxhkd .
+cp -r /home/dethereal/.config/polybar .
+mkdir tmux
+cp /home/dethereal/.tmux*  tmux/
+cp -r /home/dethereal/.config/alacritty .
