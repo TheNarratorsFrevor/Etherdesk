fcb0d284348ae13a141820ecbae7a924cd276905
Disheartenedethreal <dethereal@protonmail.com>
2022-02-10 15:10:23 +0330

    This is an automatic commit to keep the repo up to date

---
+++ conky/start_conky.sh
@@ -0,0 +1,3 @@
+conky -c ~/.config/conky/conky/configs/panels/system_panel.conkyrc
+conky -c ~/.config/conky/conky/configs/panels/network_panel.conkyrc
+
--- polybar/updates.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for a in $(ls /bin); do
-  case $a in
-    xbps-install) xbps-install -Mun | wc -l;;
-    pacman) pacman -Qu | wc -l;;
-  esac
-done
