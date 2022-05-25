1e0b15c9a55b106870822a424e5d68ccd1ff6e97
Disheartenedethreal <dethereal@protonmail.com>
2022-05-25 16:21:20 +0430

    This is an automatic commit to keep the repo up to date

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+export _JAVA_AWT_WM_NONREPARENTING=1
+alias ls="exa --long --header "
+alias lst="exa --long --header --sort=modified "
+alias lsa="exa -a --long --header --sort=modified "
+alias vim="nvim"
+alias cat="bat"
+alias wakatime-cli="wakatime"
+alias viml="nvim -S ~/.config/nvim/slime.vim"
+alias ec="emacsclient"
+export PATH="$PATH:$HOME/.spicetify"
+
--- sync_repo_with_config.sh
+++ sync_repo_with_config.sh
@@ -1,17 +1,19 @@
 # remove old configs
-rm -r i3 conky bspwm rofi fish gtk* polybar dunst kitty sxhkd .tmux.conf
+rm -r i3 conky bspwm mpd rofi ncmpcpp fish gtk* polybar dunst kitty sxhkd .tmux.conf
 # copy new configs from .config
 #cp -r ~/.config/bspwm .
-cp -r ~/.config/cmus .
+#cp -r ~/.config/cmus .
 cp -r ~/.config/rofi .
-# cp -r ~/.config/polybar .
+cp -r ~/.config/polybar .
 cp -r ~/.config/dunst .
 cp -r ~/.config/kitty .
+cp -r ~/.config/ncmpcpp .
 #cp -r ~/.config/sxhkd .
 cp -r ~/.config/fish .
 cp -r ~/.config/gtk* .
 cp -r ~/.config/i3 .
-cp -r ~/.config/conky .
+cp -r ~/.config/mpd .
+#cp -r ~/.config/conky .
 cp $HOME/.tmux.conf .
 # make an automatic commit
 git add . && git commit -m "This is an automatic commit to keep the repo up to date"
