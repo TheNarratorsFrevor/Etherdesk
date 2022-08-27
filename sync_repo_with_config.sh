7ef82a2bd0d1bebcba75ae1a01ac5685e64c7e48
Disheartenedethreal <dethereal@protonmail.com>
2022-08-27 17:51:32 +0430

    This is being done automatically.

--- sync_repo_with_config.sh
+++ sync_repo_with_config.sh
@@ -1,19 +1,21 @@
 # remove old configs
-rm -rf i3 conky bspwm nvim mpd rofi ncmpcpp fish gtk* polybar dunst kitty sxhkd .tmux.conf
+rm -rf i3 conky bspwm nvim mpd rofi ncmpcpp picom fish gtk* polybar dunst kitty sxhkd .tmux.conf
 # copy new configs from .config
-#cp -r ~/.config/bspwm .
+cp -r ~/.config/bspwm .
 #cp -r ~/.config/cmus .
 cp -r ~/.config/rofi .
 cp -r ~/.config/polybar .
 cp -r ~/.config/dunst .
 cp -r ~/.config/kitty .
 cp -r ~/.config/ncmpcpp .
-#cp -r ~/.config/sxhkd .
+cp -r ~/.config/sxhkd .
 cp -r ~/.config/fish .
 cp -r ~/.config/gtk* .
 cp -r ~/.config/i3 .
 cp -r ~/.config/mpd .
 #cp -r ~/.config/conky .
+cp -r ~/.config/picom .
+cp ~/.Xmodmap
 cp $HOME/.tmux.conf .
 # nvim stuff
 mkdir nvim
